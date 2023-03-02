083cdf6daf8d63d7576c2a8660dd69b5aa9c783d
Your Name <you@example.com>
2023-03-02 16:58:03 -0600

    Adding in batch gpu setup with build pipeline

--- Dockerfile
+++ Dockerfile
@@ -30,5 +30,5 @@ SHELL ["conda", "run", "-n", "ldm", "/bin/bash", "-c"]
 
 
 
-CMD ["pwd && ls -la && cd /home && ls -la && cd /home/ubuntu && ls -la && cd /home/ubuntu/docker && ls -la" ]
+CMD ["pwd && ls -la && cd /home && ls -la && cd /home/ubuntu && ls -la && cd /home/ubuntu/src && ls -la" ]
 #CMD ["conda activate ldm && python scripts/stable_txt2img.py --ddim_eta 0.0 --n_samples 1 --n_iter 16 --scale 10.0 --ddim_steps 50  --ckpt model.ckpt --prompt \"a beautiful tropical beach, isometric, 16bitscene\" && ls /outputs/tst-2img-sample/samples "]
--- build-ami.sh
+++ build-ami.sh
@@ -1,50 +1,32 @@
+# Works with `ami-045a50425ac09a3f5` and Instance type g4dn.xlarge
+# NOTE: I had to run this manually line by line otherwise it froze on the last line some how. ~ML 3/2/2023
 #start with the basics
-#export REGION=us-east-1
-#curl -o amazon-ecs-init.rpm https://s3.$REGION.amazonaws.com/amazon-ecs-agent-$REGION/amazon-ecs-init-latest.x86_64.rpm
-#sudo yum install -y ./amazon-ecs-init.rpm
-#sudo systemctl enable --now ecs
-#sudo systemctl status ecs
-#sudo docker ps
-#sudo systemctl stop ecs
-#sudo systemctl stop docker
+export REGION=us-east-1
+sudo apt-get update -y
+
+curl -O https://s3.us-east-1.amazonaws.com/amazon-ecs-agent-us-east-1/amazon-ecs-init-latest.amd64.deb
+sudo dpkg -i amazon-ecs-init-latest.amd64.deb
 
 
-sudo yum update -y
-
-sudo amazon-linux-extras install -y ecs; sudo systemctl enable --now ecs
-sudo yum install -y ecs-init
-
-sudo echo "ECS_ENABLE_GPU_SUPPORT=true" >> /etc/ecs/ecs.config
-sudo service ecs start
-
-sudo yum install -y nvidia-container-toolkit-base
-
-# sudo yum install -y nvidia-docker2
-sudo amazon-linux-extras install docker -y
-sudo yum install -y nvidia-container-toolkit
-sudo amazon-linux-extras install docker -y
-sudo systemctl --now enable docker
-
-sudo yum install -y nvidia-container-toolkit-base
-
-distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
-   && curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.repo | sudo tee /etc/yum.repos.d/nvidia-container-toolkit.repo
-sudo nvidia-ctk runtime configure --runtime=docker
-sudo systemctl restart docker
 
 
-  yum install -y build-essential gcc wget git libcudnn8 curl unzip && \
-  wget --quiet https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh && \
-  /bin/bash ~/miniconda.sh -b -p /opt/conda \ && \
-  export PATH=/opt/conda/bin:$PATH && \
-  curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && \
-  unzip awscliv2.zip && \
-  ./aws/install
+sudo sh -c 'echo "ECS_ENABLE_GPU_SUPPORT=true" >> /etc/ecs/ecs.config'
+
+sudo systemctl enable --now ecs # https://linuxconfig.org/how-to-start-service-on-boot-on-ubuntu-20-04 # sudo service ecs start
+
+# sudo apt-get install -y build-essential gcc wget git curl unzip && \
+wget --quiet https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh && \
+/bin/bash ~/miniconda.sh -b -p /opt/conda \ && \
+export PATH=/opt/conda/bin:$PATH && \
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && \
+unzip awscliv2.zip && \
+./aws/install
+
 git clone https://github.com/XavierXiao/Dreambooth-Stable-Diffusion
 echo "export PATH=/opt/conda/bin:$PATH" >> ~/.bashrc && \
-  echo 'conda init bash' >> ~/.bashrc  && \
-  echo 'conda activate ldm' >> ~/.bashrc
-#
+#  echo 'conda init bash' >> ~/.bashrc  && \
+#  echo 'conda activate ldm' >> ~/.bashrc
+
 mv Dreambooth-Stable-Diffusion src
 cd src
 conda env create -f ./environment.yaml -v
