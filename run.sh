e4262c4778462ac703518090fcaf90ed7bd57713
Your Name <you@example.com>
2023-03-03 16:50:53 -0600

    Adding in batch gpu setup with build pipeline

--- run.sh
+++ run.sh
@@ -5,13 +5,13 @@ echo "!!!!!!conda init bash"
 conda init bash
 
 
-echo "!!!!! cat /root/.bashrc"
-cat /root/.bashrc
+# echo "!!!!! cat /root/.bashrc"
+# cat /root/.bashrc
 
 echo "!!!!! conda activate ldm"
 conda activate ldm
 echo "!!!!Sending it. Dir: $1 Prompt: $2"
-python /home/ubuntu/src/scripts/stable_txt2img.py --outdir "$1" --ddim_eta 0.0 --n_samples 1 --n_iter 4 --scale 10.0 --ddim_steps 50  --ckpt ./model.ckpt --prompt "$2"
+python /home/ubuntu/src/scripts/stable_txt2img.py --outdir "outputs/$1" --ddim_eta 0.0 --n_samples 1 --n_iter 4 --scale 10.0 --ddim_steps 50  --ckpt ./model.ckpt --prompt "$2"
 
 echo "!!!!Pushing to S3"
 aws s3 cp /home/ubuntu/src/outputs/$1 s3://$S3_BUCKET/$1 --recursive
