b3da2b052ee073c48a18c690aa86e27e4adc4845
Your Name <you@example.com>
2023-03-11 09:50:40 -0600

    Trying to do the build on run in node

--- node/scripts/install_src.sh
+++ node/scripts/install_src.sh
@@ -1,3 +1,3 @@
-cd /home/ubuntu
-git clone https://github.com/XavierXiao/Dreambooth-Stable-Diffusion dreambooth
+cd /home/ubuntu/src
+git clone https://github.com/XavierXiao/Dreambooth-Stable-Diffusion /home/ubuntu/src/dreambooth
 # mv Dreambooth-Stable-Diffusion/* /home/ubuntu/src
