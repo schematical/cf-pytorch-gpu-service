d048ca20a54b25851cd45d69f41d21b06d6bf260
Your Name <you@example.com>
2023-03-11 07:57:50 -0600

    Trying to do the build on run in node

--- node/run.js
+++ node/run.js
@@ -7,62 +7,71 @@ const SRC_DIR = '/home/ubuntu/node';
 const CONDA_LDM_DIR = '/opt/conda/envs/ldm';
 const CONDA_DIR = '/opt/conda';
 const MODEL_PATH = '/home/ubuntu/node/model.ckpt';
+const runSpawn = async (options) => {
+    return new Promise((resolve, reject) => {
+        console.log("Did not find " + options.path + " installing");
 
+        const mainCmd = spawn(
+            options.cmd,
+            options.args
+        );
+
+        mainCmd.stdout.on('data', (data) => {
+            console.log(`stdout: ${data}`);
+        });
+
+        mainCmd.stderr.on('data', (data) => {
+            console.error(`stderr: ${data}`);
+        });
+
+        mainCmd.on('close', (code) => {
+            console.log(`child process ${options.path} exited with code ${code}`);
+            return resolve();
+        });
+    });
+}
 (async () => {
     const condaExists = fs.existsSync(CONDA_DIR);
     if (!condaExists) {
-        console.log("Did not find " + CONDA_DIR + "installing");
-        const {stdout, stderr} = await exec(`sh ${__dirname}/scripts/install_conda.sh`);
-        console.log('stdout:', stdout);
-        console.error('stderr:', stderr);
-        console.log("Done installing" + CONDA_DIR + "");
+        await runSpawn({
+            path: CONDA_DIR,
+            cmd: 'sh',
+            args: [`${__dirname}/scripts/install_conda.sh`]
+        });
     }
 
 
     const srcExists = fs.existsSync(SRC_DIR);
     if (!srcExists) {
-        console.log("Did not find " + SRC_DIR + "installing");
-        const {stdout, stderr} = await exec(`sh ${__dirname}/scripts/install_src.sh`);
-        console.log('stdout:', stdout);
-        console.error('stderr:', stderr);
-
-        console.log("Done installing" + CONDA_DIR + "");
+        await runSpawn({
+            path: SRC_DIR,
+            cmd: 'sh',
+            args: [`${__dirname}/scripts/install_src.sh`]
+        });
     }
 
 
     const condaLdmExists = fs.existsSync(CONDA_LDM_DIR);
     if (!condaLdmExists) {
-        console.log("Did not find " + CONDA_LDM_DIR + "installing");
-        const {stdout, stderr} = await exec(`sh ${__dirname}/scripts/install_src.sh`);
-        console.log('stdout:', stdout);
-        console.error('stderr:', stderr);
-
-        console.log("Done installing" + CONDA_LDM_DIR + "");
+        await runSpawn({
+            path: CONDA_LDM_DIR,
+            cmd: 'sh',
+            args: [`${__dirname}/scripts/install_src.sh`]
+        });
     }
 
     const modelExists = fs.existsSync(MODEL_PATH);
     if (!modelExists) {
-        console.log("Did not find " + MODEL_PATH + "installing");
-        const {stdout, stderr} = await exec(`sh ${__dirname}/scripts/download_model.sh`);
-        console.log('stdout:', stdout);
-        console.error('stderr:', stderr);
-
-        console.log("Done installing" + MODEL_PATH + "");
+        await runSpawn({
+            path: MODEL_PATH,
+            cmd: 'sh',
+            args: [`${__dirname}/scripts/download_model.sh`]
+        });
     }
-
-
-    const mainCmd1 = spawn("conda", ["run", "--no-capture-output", "-n", "ldm", "/bin/bash", "-c", "/home/ubuntu/node/scripts/run.sh test \"a mummy's tomb, isometric, 16bitscene\""]);
-
-    mainCmd1.stdout.on('data', (data) => {
-        console.log(`stdout: ${data}`);
-    });
-
-    mainCmd1.stderr.on('data', (data) => {
-        console.error(`stderr: ${data}`);
-    });
-
-    mainCmd1.on('close', (code) => {
-        console.log(`child process exited with code ${code}`);
+    await runSpawn({
+        path: MODEL_PATH,
+        cmd: 'conda',
+        args: ["run", "--no-capture-output", "-n", "ldm", "/bin/bash", "-c", "/home/ubuntu/node/scripts/run.sh test \"a mummy's tomb, isometric, 16bitscene\""]
     });
 // /opt/conda/envs
 })();
