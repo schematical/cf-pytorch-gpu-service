7ca221614ce6c9210b134f49fe7204aaff5f112b
schematical <mlea@schematical.com>
2023-04-19 11:12:50 -0500

    Adding new model

--- node/scripts/download_model.sh
+++ node/scripts/download_model.sh
@@ -1,2 +1,3 @@
 # Move this in to JS probablly
-aws s3 cp s3://sc-cloud-formation-v1/model.ckpt /home/ubuntu/src/model.ckpt
+# aws s3 cp s3://sc-cloud-formation-v1/model.ckpt /home/ubuntu/src/model.ckpt
+aws s3 cp s3://sc-cloud-formation-v1/schematical-3-4520.ckpt /home/ubuntu/src/model.ckpt
